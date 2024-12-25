f0ee9e3741f0b280910aaff33eee6d40a48aa54e
xavierwar <28038329@qq.com>
2024-12-25 22:20:31 +0800

    Format code

--- main.js
+++ main.js
@@ -4,18 +4,50 @@ const { app, BrowserWindow } = require('electron');
 const fs = require('fs');
 let subprocesses = [];//存储子进程
 
-const systemMessageArray = [10];
-systemMessageArray[0] = "请一般情况下使用中文回答。对用户任何回答不允许以输出代码的格式，只以输出文本的格式回复。";
-systemMessageArray[1] = "对用户的要求以代码的形式回应，每份代码前标注代码的语言，保证每次回复正确；如果用户存在某个环节未提供具体值，如任意需要的文件名，文件内容，具体的路径信息，必须要求用户完整提供而不能自己编造，且若用户提到的文件未找到，要询问用户是否新建；其中路径切记要加双斜杠；代码中所有中文转化为英文字符；如果用户返回代码或命令，则以代码的形式做出完全一样的返回。";
-systemMessageArray[2] = "衡量方案的可行性并以数字回复给用户，第一个方案数字为0，此后依次加1；仅返回方案对应的数字，返回的数字不应超过方案数量，任何情况下不返回其他。";
-systemMessageArray[3] = "请使用中文回答,复述错误并对对返回的错误进行分析";
-systemMessageArray[4] = "对返回的错误更改后以代码形式回复；其中路径切记要加双斜杠；代码中所有中文转化为英文字符；";
+const systemMessageType = {
+    DEFAULT: `请一般情况下使用中文回答。对用户任何回答不允许以输出代码的格式，只以输出文本的格式回复。`,
+    CODE: `对用户的要求以代码的形式回应，每份代码前标注代码的语言，保证每次回复正确；
 
+            如果用户存在某个环节未提供具体值，如:
+            - 任意需要的文件名
+            - 文件内容
+            - 具体的路径信息
+            必须要求用户完整提供而不能自己编造；
 
+            若用户提到的文件未找到，要询问用户是否新建；
+
+            注意事项：
+            - 路径必须使用双斜杠
+            - 代码中所有中文需转化为英文字符
+            - 如果用户返回代码或命令，则以代码的形式做出完全一样的返回`,
+    EVALUATE: `衡量方案的可行性并以数字回复给用户：
+
+                规则：
+                - 第一个方案数字为0
+                - 此后依次加1
+                - 仅返回方案对应的数字
+                - 返回的数字不应超过方案数量
+                - 任何情况下不返回其他内容`,
+    ERROR: `对返回的错误进行分析：
+
+            分析要求:
+            - 使用中文回答
+            - 分析错误原因
+            - 提供解决方案
+            - 以纯文本的形式回复`,
+    ERROR_CODE: `对返回的错误进行分析，并提供修正代码：
+
+                分析要求：
+                - 详细说明错误原因
+                - 提供具体的修复方案
+
+                代码要求：
+                - 路径必须使用双斜杠
+                - 代码中所有中文转化为英文字符
+                - 以代码形式回复修正后的完整代码`,
+};
 
 let win;
-
-
 let signal;
 let controller;
 let conversationHistory = '';
@@ -30,132 +62,129 @@ function createWindow() {
     });
 
     win.loadFile('index.html'); // 加载index.html文件
-    
+
 }
 
 app.whenReady().then(() => {
 
     createWindow();
     win.on('close', function (e) {
-    
-    if(conversationHistory==''){
-    // 阻止默认关闭事件
-    e.preventDefault();
-    aliceChatlog();
-    console.log("关闭前记录了对话历史");
-    }
-  });
+
+        if (conversationHistory == '') {
+            // 阻止默认关闭事件
+            e.preventDefault();
+            aliceChatlog();
+            console.log("关闭前记录了对话历史");
+        }
+    });
 
 });
-
-
 
 // 监听渲染进程返回的值
 ipcMain.once('renderer-value-response', (event, value, id) => {
-    const filePath = "C:\\Users\\28171\\Desktop\\aliceChatlog.txt"; // 文件路径
+    const filePath = "C:\\Users\\28038\\Desktop\\aliceChatlog.txt"; // 文件路径
     const now = new Date();
     const currentTimeString = now.toISOString();
-            if (id === 'conversationHistory') {
-                conversationHistory ="\n"+currentTimeString+"\n" + value;
-            // 写入文件
-            fs.appendFile(filePath, conversationHistory, (err) => {
-                    if (err) {
-                        appendMessage('写入文件时出错,强行退出将不保存此次历史记录:'+err,false);
-                    } else {
-                        console.log(`历史记录已保存到: ${filePath}`,false);
-                        app.quit();
-                    }
-                    
-                });
+    if (id === 'conversationHistory') {
+        conversationHistory = "\n" + currentTimeString + "\n" + value;
+        // 写入文件
+        fs.appendFile(filePath, conversationHistory, (err) => {
+            if (err) {
+                appendMessage('写入文件时出错,强行退出将不保存此次历史记录:' + err, false);
             } else {
-                console.error('接收到了错误的ID');
+                console.log(`历史记录已保存到: ${filePath}`, false);
+                app.quit();
             }
-        });
 
-//监听请求并接收
-ipcMain.on('render-send-fetch-request', (event, value, url,reqvalue) => { 
-    console.log(`接收到fetch并开始执行`);
-    sendRequest(value,url,reqvalue);
-    
+        });
+    } else {
+        console.error('接收到了错误的ID');
+    }
 });
 
+// 监听请求并接收
+ipcMain.on('render-send-fetch-request', (event, value, url, reqvalue) => {
+    console.log(`接收到fetch并开始执行`);
+    sendRequest(value, url, reqvalue);
 
-async function sendRequest(message,url,reqmessage){
-        if(!url){
-            url="https://fc.fittenlab.cn/codeapi/chat";
-        }
-        if(reqmessage){
-            reqmessage=" ";
-        }
- // 判断前缀并设置不同的系统提示词
-        let systemPrompt;
-        let userMessage;
-        let inputMessage = message.split(' ')[0]; // 获取用户输入的前缀
-        console.log(inputMessage);
-        switch (inputMessage) {
+});
 
-            case 'Alice': 
-                systemPrompt = systemMessageArray[1];
-                userMessage = message.replace(inputMessage + ' ', ''); //如果有特殊前缀则在输入中去掉，以下同
-                break;
-            case '0/1':
-                systemPrompt = systemMessageArray[2];
-                userMessage = message.replace(inputMessage + ' ', ''); 
-                break;
-            case 'default:stop':
-            case 'ds':
-                systemPrompt = systemMessageArray[3];
-                userMessage = message.replace(inputMessage + ' ', ''); 
-                break;
-            case 'default:continue':
-            case 'dc':
-                systemPrompt = systemMessageArray[4];
-                userMessage = message.replace(inputMessage + ' ', ''); 
-                break;
-            case 'exit':
-            case 'exitchatsave':
-            case 'ecs':
-                aliceChatlog();
-                break;
-            
-            default:
-                systemPrompt = systemMessageArray[0];
-                userMessage = message; 
-                break;
-        }
-        // 创建一个 AbortController 实例
-        controller = new AbortController();
+// 发送请求
+async function sendRequest(message, url, reqmessage) {
+    if (!url) {
+        url = "https://fc.fittenlab.cn/codeapi/chat";
+    }
+    if (reqmessage) {
+        reqmessage = " ";
+    }
+    // 判断前缀并设置不同的系统提示词
+    let systemPrompt;
+    let userMessage;
+    let inputMessage = message.split(' ')[0]; // 获取用户输入的前缀
+    console.log(inputMessage);
+    switch (inputMessage) {
 
-        // 获取 signal 对象
-        signal = controller.signal;
-        let abortornot;
-        signal.addEventListener('abort',
-  () => abortornot=true
-);
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-    "inputs":"<|system|>\n"+ systemPrompt+"\n<|end|>\n<|user|>\n"+reqmessage+userMessage+"\n<|end|>\n<|assistant|>",
-    "ft_token":"",
-}),
-            signal: signal
-        };
+        case 'Alice':
+            systemPrompt = systemMessageType.CODE;
+            userMessage = message.replace(inputMessage + ' ', ''); //如果有特殊前缀则在输入中去掉，以下同
+            break;
+        case '0/1':
+            systemPrompt = systemMessageType.EVALUATE;
+            userMessage = message.replace(inputMessage + ' ', '');
+            break;
+        case 'default:stop':
+        case 'ds':
+            systemPrompt = systemMessageType.ERROR;
+            userMessage = message.replace(inputMessage + ' ', '');
+            break;
+        case 'default:continue':
+        case 'dc':
+            systemPrompt = systemMessageType.ERROR_CODE;
+            userMessage = message.replace(inputMessage + ' ', '');
+            break;
+        case 'exit':
+        case 'exitchatsave':
+        case 'ecs':
+            aliceChatlog();
+            break;
 
-    userInputplaceHolder("AI响应中…") ;
-    
-    
+        default:
+            systemPrompt = systemMessageType.DEFAULT;
+            userMessage = message;
+            break;
+    }
+    // 创建一个 AbortController 实例
+    controller = new AbortController();
+
+    // 获取 signal 对象
+    signal = controller.signal;
+    let abortornot;
+    signal.addEventListener('abort',
+        () => abortornot = true
+    );
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "inputs": "<|system|>\n" + systemPrompt + "\n<|end|>\n<|user|>\n" + reqmessage + userMessage + "\n<|end|>\n<|assistant|>",
+            "ft_token": "",
+        }),
+        signal: signal
+    };
+
+    userInputplaceHolder("AI响应中…");
+
     try {
 
-        const response = await fetch(url,options).catch(error => {
-        if (error.name === 'AbortError') {
-            console.log('请求已取消');
-        } else {
-            console.error('请求出错:', error);
-        }
-    });
+        const response = await fetch(url, options).catch(error => {
+            if (error.name === 'AbortError') {
+                console.log('请求已取消');
+            } else {
+                console.error('请求出错:', error);
+            }
+        });
         if (!response.ok) {
             throw new Error('网络响应不是ok');
         }
@@ -174,7 +203,7 @@ async function sendRequest(message,url,reqmessage){
         }
 
         const aiResponse = parseJsonObjects(jsonArray);
-        
+
         // 检查首行和末行是否为 ```
         const responseLines = aiResponse.split('\n');
         const firstLine = responseLines[0].trim();
@@ -193,8 +222,8 @@ async function sendRequest(message,url,reqmessage){
                 case 'Bash':
                 case 'bash':
                     fileExtension = 'sh';
-                    inputContent +='\nread -p \'waiting…\'';
-                    shell='D:\\BaiduNetdiskDownload\\Git\\git-bash.exe';
+                    inputContent += '\nread -p \'waiting…\'';
+                    shell = 'D:\\BaiduNetdiskDownload\\Git\\git-bash.exe';
                     break;
                 case 'Python':
                 case 'python':
@@ -211,107 +240,108 @@ async function sendRequest(message,url,reqmessage){
                 case 'batch':
                     fileExtension = 'bat';
                     inputContent += "\necho 'waiting'\npause\nexit";
-                    shell='cmd';
+                    shell = 'cmd';
                     break;
                 default:
-                    sendRequest("dc "+scriptType+'是不支持的脚本类型，支持bash、python、javaScript、batch');
+                    sendRequest("dc " + scriptType + '是不支持的脚本类型，支持bash、python、javaScript、batch');
                     appendMessage('AI回复了不支持的脚本类型，正在返回ai重新生成', false);
                     userInputplaceHolder("AI重新生成命令中…");
                     return;
             }
             let fileName;
-            if(shell){
-                 fileName = inputContent;
-            }else{
-               
-            shell = process.platform === 'win32' ? process.env.COMSPEC : process.env.SHELL;
-            // 创建脚本文件
-             fileName = `aliceScript.${fileExtension}`;
-            const fs = require('fs');
-            fs.writeFileSync(fileName, inputContent);
-            }
-           
-
-
-     try{   
-        userInputplaceHolder("命令执行中…");
-    // 执行脚本并同步获取输出
-    
-    const { exec } = require('child_process');
-    let proc = exec(fileName,{shell:shell},(error, stdout, stderr) => {
-            if (error) {
-                appendMessage(`执行错误: ${error.message}`,false);
-                userInputplaceHolder("执行失败，返回错误信息给AI");
-                
-                // 如果有错误输出，将 stderr 作为错误消息
-                if (stderr) {
-                    appendMessage(`错误输出: ${stderr}`,false);
-                    sendRequest('dc ' + stderr.toString()); // 发送标准错误输出给 AI
-                } else {
-                    sendRequest('ds ' + error.message); // 发送错误消息给 AI
-                }
+            if (shell) {
+                fileName = inputContent;
             } else {
-                appendMessage(`命令执行完成\n输出:${stdout}`, false);
-                // 恢复用户输入框状态
-                userInputplaceHolder("输入内容…"); // 恢复提示
+
+                shell = process.platform === 'win32' ? process.env.COMSPEC : process.env.SHELL;
+                // 创建脚本文件
+                fileName = `aliceScript.${fileExtension}`;
+                const fs = require('fs');
+                fs.writeFileSync(fileName, inputContent);
             }
-        }); 
-        subprocesses.push(proc);
-    } catch (error) {
-        console.error(`捕获到异常: ${error.message}`);
-        userInputplaceHolder("捕获到异常，返回错误信息给AI");
-        sendRequest('ds ' + error.message); // 发送错误消息给 AI
-    }
-   
-}
- else {
+
+
+
+            try {
+                userInputplaceHolder("命令执行中…");
+                // 执行脚本并同步获取输出
+
+                const { exec } = require('child_process');
+                let proc = exec(fileName, { shell: shell }, (error, stdout, stderr) => {
+                    if (error) {
+                        appendMessage(`执行错误: ${error.message}`, false);
+                        userInputplaceHolder("执行失败，返回错误信息给AI");
+
+                        // 如果有错误输出，将 stderr 作为错误消息
+                        if (stderr) {
+                            appendMessage(`错误输出: ${stderr}`, false);
+                            sendRequest('dc ' + stderr.toString()); // 发送标准错误输出给 AI
+                        } else {
+                            sendRequest('ds ' + error.message); // 发送错误消息给 AI
+                        }
+                    } else {
+                        appendMessage(`命令执行完成\n输出:${stdout}`, false);
+                        // 恢复用户输入框状态
+                        userInputplaceHolder("输入内容…"); // 恢复提示
+                    }
+                });
+                subprocesses.push(proc);
+            } catch (error) {
+                console.error(`捕获到异常: ${error.message}`);
+                userInputplaceHolder("捕获到异常，返回错误信息给AI");
+                sendRequest('ds ' + error.message); // 发送错误消息给 AI
+            }
+
+        }
+        else {
             // 普通回复
             appendMessage(aiResponse, false);
             // 恢复用户输入框状态
-             
+
             userInputplaceHolder("输入内容…"); // 恢复提示
         }
     } catch (error) {
         console.error('请求出错:', error);
         if (abortornot) {
-            abortornot=false;
-            appendMessage('会话已取消',false);
-        }else{
-        appendMessage('AI正在睡觉.zZ', false);
+            abortornot = false;
+            appendMessage('会话已取消', false);
+        } else {
+            appendMessage('AI正在睡觉.zZ', false);
         }// 恢复用户输入框状态
-         
+
         userInputplaceHolder("输入内容…"); // 恢复提示
     }
 }
-function aliceChatlog(){
-     // 当窗口加载完成后发送请求到渲染进程
-          win.webContents.send('main-get-value-request',{elementId:'conversationHistory'});
+function aliceChatlog() {
+    // 当窗口加载完成后发送请求到渲染进程
+    win.webContents.send('main-get-value-request', { elementId: 'conversationHistory' });
 }
 
-function userInputplaceHolder(text){
-win.webContents.send('main-holder-text-change',{text:text});
-}
-function appendMessage(aiResponse,value){
-win.webContents.send('main-append-message',{aiResponse:aiResponse,value:value});
+function userInputplaceHolder(text) {
+    win.webContents.send('main-holder-text-change', { text: text });
 }
 
-ipcMain.on('interrupt-subprocesses',(event)=>{
+function appendMessage(aiResponse, value) {
+    win.webContents.send('main-append-message', { aiResponse: aiResponse, value: value });
+}
+
+ipcMain.on('interrupt-subprocesses', (event) => {
     console.log("接收到中断的请求");
     //console.log("当前进程："+subprocesses[0]);
     // 取消fetch请求
-    if(controller){
+    if (controller) {
         console.log("存在可取消的fetch");
     }
     controller.abort();
     //取消子进程
     subprocesses.forEach((proc) => {
-                try {
-                    //console.log("已中断");
-                    proc.kill('SIGINT'); // 安全地中断子进程
-                } catch (err) {
-                    console.error(`中断子进程时出错: ${err}`);
-                }
-            });
-            subprocesses = []; // 清空子进程数组
+        try {
+            //console.log("已中断");
+            proc.kill('SIGINT'); // 安全地中断子进程
+        } catch (err) {
+            console.error(`中断子进程时出错: ${err}`);
+        }
+    });
+    subprocesses = []; // 清空子进程数组
 }
 );
